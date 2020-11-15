30bbda8961eb118aacd90b8ae9f733b0c31870be
flores-o <oana.florescu28@gmail.com>
2020-11-15 15:20:44 +0000

    sometime our extension work, sometimes it does not; we consol logged everything and added comments

--- content.js
+++ content.js
@@ -1,9 +1,26 @@
 console.log("Am I running this script");
 
-$("[aria-label=Conversations]").css('visibility', 'hidden')
+$('document').ready(function(){
+ console.log("document loaded")
+ if ($("[aria-label=Conversations]"))
+     console.log("We can select the element that has aria-label=Conversations");
+ else
+     console.log("We cannot select the elem that has aria-label=Conversations");
+ try
+ {
+     console.log($("[aria-label=Conversations]").css('visibility', 'hidden'))
+     
+ }
+ catch(err)
+ {  
+     console.log("trying to set the css property threw an error")
+     console.log(err)
+ }
+ if (($("[aria-label=Conversations]").css('visibility')) == 'hidden')
+     console.log("Our extension changed the css")
+ else
+     console.log("Our extension did not change the css")
 
-$('document').ready(function(){ 
- $("[aria-label=Conversations]").css('visibility', 'hidden')
  });
 
 
